37c1cc1f117535c19bf708d91d5c979219b1c532
Thomas Maas <t.p.maas@student.utwente.nl>
2026-05-16 19:49:39 +0300

    Changed game duration

--- src/components/GameWindow.tsx
+++ src/components/GameWindow.tsx
@@ -17,7 +17,7 @@ import TradeControls, {
 } from "./TradeControls";
 
 const DRIP_SPEED_MS = 500;
-const GAME_DURATION_S = 5;
+const GAME_DURATION_S = 30;
 const INITIAL_VISIBLE = 60;
 
 type GameState = "IDLE" | "PLAYING" | "GAME_OVER";
